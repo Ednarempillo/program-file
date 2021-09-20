1072921cc089ee00e727a79af0f0fa4a86136d4c
Ednarempillo <90894746+Ednarempillo@users.noreply.github.com>
2021-09-20 16:25:13 +0800

    Update program1.py

--- program1.py
+++ program1.py
@@ -6,9 +6,9 @@ semifinal = float(input("Enter Score: "))
 final = float(input("Enter Score: "))
 
 average = (prelim + midterm + semifinal + final)/4
+
 print("Average grade: {}!".format(average))
 
-print("Average grade: ")
 if average >=75:
     print("You PASSED.")
 elif average <75:
