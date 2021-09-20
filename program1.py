77fa47b232e63c22ecbcd0454488aac935de141d
Edna <ednarempillo100@gmail.com>
2021-09-20 08:09:15 +0000

    program1Branch

---
+++ program1.py
@@ -0,0 +1,14 @@
+print("This program will compute and print student average score")
+
+prelim = float(input("Enter Score: "))
+midterm = float(input("Enter Score: "))
+semifinal = float(input("Enter Score: "))
+final = float(input("Enter Score: "))
+
+average = (prelim + midterm + semifinal + final)/4
+
+print("Average grade: ")
+if average >=75:
+    print("You PASSED.")
+elif average <75:
+    print("You FAILED.")
